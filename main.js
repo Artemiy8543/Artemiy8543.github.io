1945905fe58b989be81189fceb610db226f9d9c6
Artemiy8543 <artemijmincakov31491@gmail.com>
2024-08-07 03:41:54 +0700

    start

---
+++ main.js
@@ -0,0 +1,28 @@
+const API_KEY = 'F97155F15091531BCEC721143B9DD638';
+
+async function getAvatarUrl(steamId){
+    const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${steamId}`;
+
+    try {
+        const response = await fetch(url);
+
+        return response;
+    } catch (error) {
+        console.error('Ошибка при получении:', error);
+    }
+}
+
+const Http = new XMLHttpRequest();
+const data_url="https://raw.githubusercontent.com/Artemiy8543/Leaderbords/master/data.txt";
+Http.open("GET", data_url);
+Http.send();
+Http.onreadystatechange = (e) => {
+  const text = Http.responseText;
+  if(text==""){
+    return
+  }
+  const rank = text.slice(text.indexOf(',')+1,text.indexOf('-'));
+  const steamid = text.slice(0,text.indexOf(','));
+
+  getAvatarUrl(steamid);
+}
