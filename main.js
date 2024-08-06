5717f7be4409059844b81d4b608a77b78fcc6fdd
Artemiy8543 <artemijmincakov31491@gmail.com>
2024-08-07 05:12:01 +0700

    update

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 const API_KEY = 'F97155F15091531BCEC721143B9DD638';
 
-async function getAvatarUrl(steamId){
-    const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${steamId}`;
+async function getAvatarUrl(steamId, rank){
+    const leaderboard = document.getElementById('leaderbords');
+    const url = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${API_KEY}&steamids=${steamId}`;
 
-    try {
-        const response = await fetch(url, {
-                                           mode: 'no-cors',
+    const request = await fetch(url, {
                                            method: "GET",
+                                           headers: {
+                                               "origin": "0",
+                                               "x-requested-with":"0",
+                                           },
                                          });
+    const data = await request.json();
+    const leaderDiv = document.createElement('div');
+    leaderDiv.className = 'player';
 
-        return response;
-    } catch (error) {
-        console.error('Ошибка при получении:', error);
-    }
+    const image = document.createElement('img');
+    image.className = "avatar";
+    image.src = data.response.players[0].avatarmedium;
+    leaderDiv.appendChild(image);
+
+    const name = document.createElement('h1');
+    name.className = "username";
+    name.textContent = data.response.players[0].personaname;
+    leaderDiv.appendChild(name);
+
+    const mmr = document.createElement('h1');
+    mmr.className = "rank";
+    mmr.textContent = rank;
+    leaderDiv.appendChild(mmr);
+
+    leaderboard.appendChild(leaderDiv);
 }
 
 const Http = new XMLHttpRequest();
@@ -20,12 +38,15 @@ const data_url="https://raw.githubusercontent.com/Artemiy8543/Leaderbords/master
 Http.open("GET", data_url);
 Http.send();
 Http.onreadystatechange = (e) => {
-  const text = Http.responseText;
+  const data = Http.responseText;
+  text = data.replace(/\n/g, "");
   if(text==""){
     return
   }
-  const rank = text.slice(text.indexOf(',')+1,text.indexOf('-'));
-  const steamid = text.slice(0,text.indexOf(','));
-
-  getAvatarUrl(steamid);
+  while(text.indexOf(";")!=-1){
+      const rank = text.slice(text.indexOf(',')+1,text.indexOf('-'));
+      const steamid = text.slice(0,text.indexOf(','));
+      text = text.slice(text.indexOf(';')+1);
+      getAvatarUrl(steamid, rank);
+  }
 }
