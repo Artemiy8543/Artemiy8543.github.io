e301632dc890c88b6ac2dbe4a85ef306427b36cf
Artemiy8543 <artemijmincakov31491@gmail.com>
2024-09-13 23:08:36 +0700

    update

--- School_Project/program.js
+++ School_Project/program.js
@@ -3,7 +3,10 @@ function addListeners(request) {
 }
 
 async function main(url){
-    const request = await fetch(url);
+    const request = await fetch(url, {method: 'GET',
+                                        headers: {
+                                            'Authorization': 'Bearer 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'
+                                        }});
     if(request.status != 200)return;
     const data = await request.json();
     const program_name = document.getElementById('program-name');
